0fba330aaf7de51878866fb8e4e166561d33b428
Laís Tomaz <lais@eokoe.com>
2018-02-21 16:33:37 -0300

    Add Merchant methods

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,29 @@
 /* eslint-disable no-undef, arrow-body-style, no-param-reassign, no-console */
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
+
+import config from '../config';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
+    programs: [],
   },
   actions: {
+    LOAD_MERCHANTS({ commit }) {
+      axios.get(`${config.api}/public/merchant-programs`).then((response) => {
+        commit('SET_MERCHANTS', { res: response.data });
+      }, (err) => {
+        console.error(err);
+      });
+    },
   },
   mutations: {
+    SET_MERCHANTS(state, { res }) {
+      state.programs = res[0].merchant_programs;
+    },
   },
 });
 
