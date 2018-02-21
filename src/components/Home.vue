d67de9b9360d7a7a857afa70b0834dd35c3b8ed3
Laís Tomaz <lais@eokoe.com>
2018-02-21 16:33:24 -0300

    Create Home component

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Rede</h1>
+
+  <h2>Programs</h2>
+  <ul>
+    <li v-for="program in programs">{{ program.name}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  computed: {
+    programs() {
+      return this.$store.state.programs;
+    },
+  },
+  mounted() {
+    this.$store.dispatch('LOAD_MERCHANTS');
+  },
+};
+</script>
