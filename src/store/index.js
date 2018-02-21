a0df82e9e6f42535a8e9e4a4343f8ba5fc7e88c9
Laís Tomaz <lais@eokoe.com>
2018-02-21 16:14:24 -0300

    Init Vuex store

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef, arrow-body-style, no-param-reassign, no-console */
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+  },
+  actions: {
+  },
+  mutations: {
+  },
+});
+
+export default store;
