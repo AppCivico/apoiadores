9732c7faac98ae48089c9c1832fcac2cb5c30ad6
Laís Tomaz <lais@eokoe.com>
2018-02-22 10:36:57 -0300

    Enhance home page to show list of programs

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,28 @@
 <template>
-  <h1>Rede</h1>
+	<div>
+		<h1>Rede</h1>
 
-  <h2>Programs</h2>
-  <ul>
-    <li v-for="program in programs">{{ program.name}}</li>
-  </ul>
+		<h2>Programs</h2>
+		<ul>
+			<li v-for="program in programs" :key="program.id">
+				<router-link :to="`/program/${program.id}`">
+					{{ program.name}}
+				</router-link>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-  computed: {
-    programs() {
-      return this.$store.state.programs;
-    },
-  },
-  mounted() {
-    this.$store.dispatch('LOAD_MERCHANTS');
-  },
+	name: 'Home',
+	computed: {
+		programs() {
+			return this.$store.state.programs;
+		},
+	},
+	mounted() {
+		this.$store.dispatch('LOAD_MERCHANTS');
+	},
 };
 </script>
