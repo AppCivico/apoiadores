114b89cb79fe2465ab717e7b9c040397ff90bdbb
Laís Tomaz <lais@eokoe.com>
2018-02-22 16:39:14 -0300

    Enhance Login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,54 @@
 <template>
 	<div>
 		<h3>Login component</h3>
-		<p>Soon</p>
+		<form @submit.prevent="validateForm">
+			<label for="email">Email</label>
+			<input type="email" v-model="email" name="email">
+			<label for="password">Senha</label>
+			<input type="password" v-model="password" name="password">
+			<button type="submit">Entrar</button>
+		</form>
 	</div>
 </template>
 
 <script>
+import { validate } from '../utilities';
+
 export default {
 	name: 'Login',
+	props: {
+		route: String,
+	},
+	data() {
+		return {
+			email: '',
+			password: '',
+		};
+	},
+	computed: {
+		merchant() {
+			return this.$store.state.merchant;
+		},
+	},
+	methods: {
+		validateForm() {
+			const { email, password } = this;
+			const fields = { email, password };
+			const validation = validate(fields);
+
+			if (validation.valid) {
+				fields.merchant_id = this.merchant.id;
+				this.login(fields);
+			} else {
+				console.error('formulario contem erros', validation.errors);
+			}
+		},
+		login(data) {
+			this.$store.dispatch('LOGIN', data)
+				.then(() => {
+					this.$router.push({ path: this.route });
+				});
+		},
+	},
 };
 </script>
