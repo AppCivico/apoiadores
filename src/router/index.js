6c54a4e0ce8f5528aadaf452f87f0c7f7a4e4f70
Laís Tomaz <lais@eokoe.com>
2018-02-22 12:29:49 -0300

    Add Program to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+
 import Home from '@/components/Home';
+import Program from '@/components/steps/Program';
 
 Vue.use(Router);
 
@@ -10,6 +12,12 @@ export default new Router({
 			path: '/',
 			name: 'Home',
 			component: Home,
+		},
+		{
+			path: '/program/:id',
+			name: 'Program',
+			component: Program,
+			props: true,
 		},
 	],
 });
