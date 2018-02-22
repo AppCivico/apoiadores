c7b20e3775cc2a4eecbca4e358c8c50ab7d0e182
Laís Tomaz <lais@eokoe.com>
2018-02-22 10:21:45 -0300

    Use tabs instead of spaces

--- src/store/index.js
+++ src/store/index.js
@@ -8,23 +8,23 @@ import config from '../config';
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-  state: {
-    programs: [],
-  },
-  actions: {
-    LOAD_MERCHANTS({ commit }) {
-      axios.get(`${config.api}/public/merchant-programs`).then((response) => {
-        commit('SET_MERCHANTS', { res: response.data });
-      }, (err) => {
-        console.error(err);
-      });
-    },
-  },
-  mutations: {
-    SET_MERCHANTS(state, { res }) {
-      state.programs = res[0].merchant_programs;
-    },
-  },
+	state: {
+		programs: [],
+	},
+	actions: {
+		LOAD_MERCHANTS({ commit }) {
+			axios.get(`${config.api}/public/merchant-programs`).then((response) => {
+				commit('SET_MERCHANTS', { res: response.data });
+			}, (err) => {
+				console.error(err);
+			});
+		},
+	},
+	mutations: {
+		SET_MERCHANTS(state, { res }) {
+			state.programs = res[0].merchant_programs;
+		},
+	},
 });
 
 export default store;
