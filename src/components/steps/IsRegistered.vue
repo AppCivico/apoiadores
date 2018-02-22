d5bcc9fb6440a17cfbf968b47973c11bf59c7573
Laís Tomaz <lais@eokoe.com>
2018-02-22 18:20:44 -0300

    Make eslint happy

--- src/components/steps/IsRegistered.vue
+++ src/components/steps/IsRegistered.vue
@@ -7,7 +7,10 @@
 		<hr>
 
 		<h2>Você é novo por aqui?</h2>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo cupiditate, quasi provident a iusto dignissimos, debitis nostrum repellat! Architecto libero laudantium tempore deserunt totam quaerat fugit harum, sequi doloribus odit!</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo cupiditate,
+		quasi provident a iusto dignissimos, debitis nostrum repellat!
+		Architecto libero laudantium tempore deserunt totam quaerat fugit harum,
+		sequi doloribus odit!</p>
 		<router-link to="/register">Cadastre-se</router-link>
 	</div>
 </template>
