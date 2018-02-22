4ba13097c21c8cb19a247f9af5346fdfcf0564b7
Laís Tomaz <lais@eokoe.com>
2018-02-22 13:37:31 -0300

    Enhance isRegistered component

--- src/components/steps/IsRegistered.vue
+++ src/components/steps/IsRegistered.vue
@@ -2,11 +2,23 @@
 	<div>
 		<h1>Is Registered</h1>
 
+		<Login />
+
+		<hr>
+
+		<h2>Você é novo por aqui?</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo cupiditate, quasi provident a iusto dignissimos, debitis nostrum repellat! Architecto libero laudantium tempore deserunt totam quaerat fugit harum, sequi doloribus odit!</p>
+		<router-link to="/register">Cadastre-se</router-link>
 	</div>
 </template>
 
 <script>
+import Login from '../Login';
+
 export default {
 	name: 'IsRegistered',
+	components: {
+		Login,
+	},
 };
 </script>
