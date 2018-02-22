ca63168af537ca624508dfe84a82e1492d52f4fb
Laís Tomaz <lais@eokoe.com>
2018-02-22 18:03:51 -0300

    Enhance Finish component

--- src/components/steps/Finish.vue
+++ src/components/steps/Finish.vue
@@ -1,11 +1,24 @@
 <template>
 	<div>
 		<h1>Finish</h1>
+		<h2>Contribuição realizada com sucesso!</h2>
+		<p>Você contribuiu com o valor de R$ {{ donation.amount }} para a {{ name }}</p>
+		<router-link to="/">voltar a página inicial</router-link>
 	</div>
 </template>
 
 <script>
+import config from '../../config';
+
 export default {
 	name: 'Finish',
+	computed: {
+		donation() {
+			return this.$store.state.donation;
+		},
+		name() {
+			return config.name;
+		},
+	},
 };
 </script>
