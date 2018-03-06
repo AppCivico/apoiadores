209ca3af8d45124e2da5afebfe3f97bd9fb5f75f
Laís Tomaz <lais@eokoe.com>
2018-03-06 14:25:18 -0300

    Import main stylesheet to App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,18 +10,4 @@ export default {
 };
 </script>
 
-<style>
-#app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	color: #2c3e50;
-	width: 100%;
-	max-width: 960px;
-	margin: 60px auto 0;
-}
-
-input {
-	display: block;
-}
-</style>
+<style src="./stylesheets/index.scss" lang="scss"></style>
