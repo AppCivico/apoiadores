69fcd5ba3b5581f59ae3829d8698a78033b1975a
Laís Tomaz <lais@eokoe.com>
2018-03-08 17:17:07 -0300

    Edit config file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-import logoApoiadores from './assets/img/partners/logo_apoiadores.png';
-import logoAppCivico from './assets/img/partners/logo_appcivico.png';
+import logoApoiadores from './assets/img/customization/partners/logo_apoiadores.png';
+import logoAppCivico from './assets/img/customization/partners/logo_appcivico.png';
 
 const config = {
 	api: 'https://dev-apoiadores-api.eokoe.com',
@@ -20,17 +20,28 @@ const config = {
 	content: {
 		header: {
 			title: 'MUITOS<strong>DOANDO</strong><span>POUCO</span>',
-			text: 'A REDE está ciente de que sua força organizativa e sua contribuição política serão mais inspiradoras e sustentáveis na medida em que forem o reflexo de uma cultura verdadeira e de práticas autênticas dos que se dispõe a liderar. Participe!',
+			text: 'A REDE está ciente de que sua força organizativa e sua contribuição política serão mais inspiradoras e sustentáveis na medida em que forem o reflexo de uma cultura verdadeira e de práticas autênticas dos que se dispõe a liderar. <strong>Participe</strong>!',
 		},
-		boxes: {
-			project: 'A democracia precisa de cidadãos e cidadãs responsáveis, bem informados e bem formados',
-			service: 'A democracia precisa de cidadãos e cidadãs responsáveis, bem informados e bem formados',
-			donation: 'A democracia precisa de cidadãos e cidadãs responsáveis, bem informados e bem formados',
+		programs: {
+			project: {
+				title: 'Compartilhe um <strong>Sonho</strong>',
+				text: 'A democracia precisa de cidadãos e cidadãs responsáveis, bem informados e bem formados',
+				active: false,
+			},
+			service: {
+				title: 'Doe seu <strong>Serviço</strong>',
+				text: 'A democracia precisa de cidadãos e cidadãs responsáveis, bem informados e bem formados',
+				active: false,
+			},
+			donation: {
+				title: 'Faça uma <strong>Doação</strong>',
+				text: 'A democracia precisa de cidadãos e cidadãs responsáveis, bem informados e bem formados',
+				active: true,
+			},
 		},
 		video: {
 			title: 'Um chamado ao <strong>engajamento</strong>',
-			subtitle: 'XXXX pessoas já doaram, <a href="#donation">seja a próxima</a>',
-			url: 'https://www.youtube.com/watch?v=Pqs6-rLmqWI',
+			id: 'Pqs6-rLmqWI',
 			text: 'Ouça o que Marina Silva tem a dizer sobre engajamento. A REDE está ciente de que sua força organizativa e sua contribuição política serão mais inspiradoras e sustentáveis na medida em que forem o reflexo de uma cultura verdadeira e de práticas autênticas dos que se dispõe a liderar.',
 		},
 	},
