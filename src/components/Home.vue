6223a2581a3dfdb7c89b32340758a1db1e37e1a4
Laís Tomaz <lais@eokoe.com>
2018-03-08 17:18:29 -0300

    Improve Home component structure

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,31 +1,90 @@
 <template>
-	<div>
-		<h2>Programs</h2>
-		<ul>
-			<li v-for="program in programs" :key="program.id" @click="setCurrentProgram(program)">
-				{{ program.name}}
-			</li>
-		</ul>
-	</div>
+	<main>
+		<section class="slogan">
+			<div class="container">
+				<h3>Faça sua Doação</h3>
+				<h2 v-html="header.title"></h2>
+				<p v-html="header.text"></p>
+			</div>
+		</section>
+		<section class="programs container">
+			<div class="program" v-for="(program, key) in programs">
+				<h3 v-html="program.title"></h3>
+				<p>{{ program.text }}</p>
+				<a
+					v-if="key === 'donation'"
+					href="#"
+					@click.prevent="setCurrentProgram(program.active ? donation : null)"
+				>
+					{{ program.active ? 'DOE AGORA!' : 'EM BREVE' }}
+				</a>
+				<a
+					v-else-if="key === 'project'"
+					href="#"
+					@click.prevent="setCurrentProgram(program.active ? '' : null)"
+				>
+					{{ program.active ? 'DOE AGORA!!' : 'EM BREVE' }}
+				</a>
+				<a
+					v-else
+					href="#"
+					@click.prevent="setCurrentProgram(program.active ? '' : null)"
+				>
+					{{ program.active ? 'DOE AGORA!' : 'EM BREVE' }}
+				</a>
+			</div>
+		</section>
+		<section class="video">
+			<div class="container">
+				<a
+					:href="`https://www.youtube.com/watch?v=${video.id}`"
+					target="_blank"
+					class="video__link"
+				>
+					<img :src="`https://i.ytimg.com/vi/${video.id}/maxresdefault.jpg`" :alt="video.text">
+				</a>
+				<h2 v-html="video.title"></h2>
+				<h3>
+						{{ donation.summary.captured_count }}
+						pessoas já doaram,
+						<a href="#" @click.prevent="setCurrentProgram(donation)">
+							seja a próxima
+						</a>
+				</h3>
+				<p>{{ video.text }}</p>
+			</div>
+		</section>
+	</main>
 </template>
 
 <script>
+import config from '../config';
+
 export default {
 	name: 'Home',
 	computed: {
-		programs() {
-			return this.$store.state.programs;
+		donation() {
+			return this.$store.state.programs[0];
 		},
+	},
+	data() {
+		return {
+			header: config.content.header,
+			video: config.content.video,
+			programs: config.content.programs,
+		};
 	},
 	mounted() {
 		this.$store.dispatch('LOAD_MERCHANTS');
 	},
 	methods: {
 		setCurrentProgram(program) {
-			this.$store.dispatch('CHANGE_SELECTED_PROGRAM', program)
-				.then(() => {
-					this.$router.push({ path: `/program/${program.id}` });
-				});
+			if (program) {
+				this.$store.dispatch('CHANGE_SELECTED_PROGRAM', program)
+					.then(() => {
+						this.$router.push({ path: `/program/${program.id}` });
+					});
+			}
 		},
 	},
 };
