14864edef70b5b7484713d15787fff7af17455c6
Laís Tomaz <laisotomaz@gmail.com>
2018-03-16 18:47:50 -0300

    Remove autoplay from Youtube embed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -74,7 +74,6 @@
 					height="315"
 					:src="`https://www.youtube.com/embed/${video.id}?rel=0&amp;showinfo=0`"
 					frameborder="0"
-					allow="autoplay; encrypted-media"
 					allowfullscreen>
 				</iframe>
 			</div>
