ec304244d353a89bda03c4d1601dd58712e047ed
Laís Tomaz <lais@eokoe.com>
2018-03-28 20:01:21 -0300

    Change projects properties to match api

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -5,10 +5,10 @@
 			<h3>Escolha um projeto e ajude</h3>
 			<div class="project" v-for="project in hardCoded" :key="project.id">
 				<span>{{ getPercentage(project) }}%</span>
-				<h4>{{ project.title }}</h4>
+				<h4>{{ project.name }}</h4>
 				<p>{{ project.description }}</p>
 				<span>{{ project.count }} pessoas já ajudaram</span>
-				<router-link :to="`/projects/${project.id}`"></router-link>
+				<router-link :to="`/projects/${project.id}`">QUERO AJUDAR</router-link>
 			</div>
 		</section>
 	</main>
@@ -22,26 +22,31 @@ export default {
 			hardCoded: [
 				{
 					id: 1,
-					title: 'Sozinhas podemos ser gotas, mas juntas somos um oceano',
+					name: 'Sozinhas podemos ser gotas, mas juntas somos um oceano',
 					description: 'Vivamus finibus neque vitae diam efficitur, nec tempus augue hendrerit. Vestibulum condimentum ullamcorper turpis, ut auctor nulla facilisis at. Finibus neque vitae diam efficitur, nec tempus augue hendrerit. Vestibulum condimentum ullamcorper turpis, ut auctor nulla facilisis at.',
 					count: 2299,
 					amount: 50,
-					target: 200,
+					goal: 200,
 				},
 				{
 					id: 2,
-					title: 'Sozinhas podemos ser gotas, mas juntas somos um oceano',
+					name: 'Sozinhas podemos ser gotas, mas juntas somos um oceano',
 					description: 'Vivamus finibus neque vitae diam efficitur, nec tempus augue hendrerit. Vestibulum condimentum ullamcorper turpis, ut auctor nulla facilisis at. Finibus neque vitae diam efficitur, nec tempus augue hendrerit. Vestibulum condimentum ullamcorper turpis, ut auctor nulla facilisis at.',
 					count: 100,
 					amount: 50,
-					target: 100,
+					goal: 100,
 				},
 			],
 		};
 	},
+	computed: {
+		projects() {
+			return this.$store.state.projects;
+		},
+	},
 	methods: {
 		getPercentage(project) {
-			return (project.amount * 100) / project.target;
+			return (project.amount * 100) / project.goal;
 		},
 	},
 };
