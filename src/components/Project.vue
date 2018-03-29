e528c8afeba9788e5fe6ee6ef90f87640b1d43df
Laís Tomaz <lais@eokoe.com>
2018-03-29 18:16:48 -0300

    Enhance project component

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,7 +1,22 @@
 <template>
-	<main class="container">
+	<main class="container project">
 		<section class="content">
-			<h2>Project {{ id }}</h2>
+			<h2>COMPARTILHE UM SONHOS</h2>
+			<h3>{{ project.name }}</h3>
+			<header>
+				<div :style="{ backgroundImage: 'url(https://78.media.tumblr.com/tumblr_m2q8lrcojk1qfvoueo1_1280.jpg)'}"></div>
+				<div class="project__header">
+					<p><strong>{{ project.count }}</strong> pessoas já ajudaram</p>
+					<p><strong>{{ project.count }}</strong> dias restantes</p>
+
+					<div class="project__percentage">
+						{{ getPercentage(project) }}%
+						<span
+							:style="{ width: `${getPercentage(project)}%`}"
+						></span>
+					</div>
+				</div>
+			</header>
 		</section>
 	</main>
 </template>
@@ -11,6 +26,23 @@ export default {
 	name: 'Projects',
 	props: {
 		id: String,
+	},
+	data() {
+		return {
+			project: {
+				id: 4,
+				name: 'Sozinhas podemos ser gotas, mas juntas somos um oceano',
+				description: 'Vivamus finibus neque vitae diam efficitur, nec tempus augue hendrerit. Vestibulum condimentum ullamcorper turpis, ut auctor nulla facilisis at. Finibus neque vitae diam efficitur, nec tempus augue hendrerit. Vestibulum condimentum ullamcorper turpis, ut auctor nulla facilisis at.',
+				count: 100,
+				amount: 50,
+				goal: 100,
+			},
+		};
+	},
+	methods: {
+		getPercentage(project) {
+			return (project.amount * 100) / project.goal;
+		},
 	},
 };
 </script>
