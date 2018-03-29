ad64a407a41f70893aa237989b00639ad4fd3597
Laís Tomaz <lais@eokoe.com>
2018-03-29 18:17:03 -0300

    Change class names in Project component

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,9 +4,9 @@
 			<h2>Compartilhe um sonho</h2>
 			<h3>Escolha um projeto e ajude</h3>
 
-			<div class="projects__project project__main">
+			<div class="projects__project projects__main">
 				<div
-					class="project__thumbnail"
+					class="projects__thumbnail"
 					:style="{ backgroundImage: 'url(https://78.media.tumblr.com/tumblr_m2q8lrcojk1qfvoueo1_1280.jpg)'}"
 				>
 					<div class="thumbnail__percentage">
@@ -16,7 +16,7 @@
 						></span>
 					</div>
 				</div>
-				<div class="project__content">
+				<div class="projects__content">
 					<h4>{{ mainProject.name }}</h4>
 					<p>{{ mainProject.description }}</p>
 					<router-link :to="`/projects/${mainProject.id}`">QUERO AJUDAR</router-link>
@@ -26,7 +26,7 @@
 			<div class="projects__row">
 				<div class="projects__project" v-for="project in hardCoded" :key="project.id">
 					<div
-						class="project__thumbnail"
+						class="projects__thumbnail"
 						:style="{ backgroundImage: 'url(https://78.media.tumblr.com/tumblr_m2q8lrcojk1qfvoueo1_1280.jpg)'}"
 					>
 						<div class="thumbnail__percentage">
@@ -36,7 +36,7 @@
 							></span>
 						</div>
 					</div>
-					<div class="project__content">
+					<div class="projects__content">
 						<h4>{{ project.name }}</h4>
 						<p>{{ project.description }}</p>
 						<span>{{ project.count }} pessoas já ajudaram</span>
@@ -64,6 +64,22 @@ export default {
 				},
 				{
 					id: 2,
+					name: 'Sozinhas podemos ser gotas, mas juntas somos um oceano',
+					description: 'Vivamus finibus neque vitae diam efficitur, nec tempus augue hendrerit. Vestibulum condimentum ullamcorper turpis, ut auctor nulla facilisis at. Finibus neque vitae diam efficitur, nec tempus augue hendrerit. Vestibulum condimentum ullamcorper turpis, ut auctor nulla facilisis at.',
+					count: 100,
+					amount: 50,
+					goal: 100,
+				},
+				{
+					id: 4,
+					name: 'Sozinhas podemos ser gotas, mas juntas somos um oceano',
+					description: 'Vivamus finibus neque vitae diam efficitur, nec tempus augue hendrerit. Vestibulum condimentum ullamcorper turpis, ut auctor nulla facilisis at. Finibus neque vitae diam efficitur, nec tempus augue hendrerit. Vestibulum condimentum ullamcorper turpis, ut auctor nulla facilisis at.',
+					count: 100,
+					amount: 50,
+					goal: 100,
+				},
+				{
+					id: 5,
 					name: 'Sozinhas podemos ser gotas, mas juntas somos um oceano',
 					description: 'Vivamus finibus neque vitae diam efficitur, nec tempus augue hendrerit. Vestibulum condimentum ullamcorper turpis, ut auctor nulla facilisis at. Finibus neque vitae diam efficitur, nec tempus augue hendrerit. Vestibulum condimentum ullamcorper turpis, ut auctor nulla facilisis at.',
 					count: 100,
