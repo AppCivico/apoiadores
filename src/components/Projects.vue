db594d3bb8f5c7f4fe997f2f0bee3723691f1351
Laís Tomaz <lais@eokoe.com>
2018-03-29 15:29:29 -0300

    Add main project structure and style

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,6 +3,26 @@
 		<section class="content">
 			<h2>Compartilhe um sonho</h2>
 			<h3>Escolha um projeto e ajude</h3>
+
+			<div class="projects__project project__main">
+				<div
+					class="project__thumbnail"
+					:style="{ backgroundImage: 'url(https://78.media.tumblr.com/tumblr_m2q8lrcojk1qfvoueo1_1280.jpg)'}"
+				>
+					<div class="thumbnail__percentage">
+						{{ getPercentage(mainProject) }}%
+						<span
+							:style="{ width: `${getPercentage(mainProject)}%`}"
+						></span>
+					</div>
+				</div>
+				<div class="project__content">
+					<h4>{{ mainProject.name }}</h4>
+					<p>{{ mainProject.description }}</p>
+					<router-link :to="`/projects/${mainProject.id}`">QUERO AJUDAR</router-link>
+				</div>
+			</div>
+
 			<div class="projects__row">
 				<div class="projects__project" v-for="project in hardCoded" :key="project.id">
 					<div
@@ -16,7 +36,7 @@
 							></span>
 						</div>
 					</div>
-					<div class="projects__content">
+					<div class="project__content">
 						<h4>{{ project.name }}</h4>
 						<p>{{ project.description }}</p>
 						<span>{{ project.count }} pessoas já ajudaram</span>
@@ -51,6 +71,14 @@ export default {
 					goal: 100,
 				},
 			],
+			mainProject: {
+				id: 3,
+				name: 'Sozinhas podemos ser gotas, mas juntas somos um oceano',
+				description: 'Vivamus finibus neque vitae diam efficitur, nec tempus augue hendrerit. Vestibulum condimentum ullamcorper turpis, ut auctor nulla facilisis at. Finibus neque vitae diam efficitur, nec tempus augue hendrerit. Vestibulum condimentum ullamcorper turpis, ut auctor nulla facilisis at.',
+				count: 100,
+				amount: 50,
+				goal: 100,
+			},
 		};
 	},
 	computed: {
