168790982d4705b798aa3629a408dbed754b98f7
Laís Tomaz <lais@eokoe.com>
2018-04-24 17:11:05 -0300

    Change HTML structure to improve semantics

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,14 +1,14 @@
 <template>
 	<main class="container project theme_project">
-		<section class="content" v-if="project">
+		<article class="content" v-if="project">
 			<h2>COMPARTILHE UM SONHO</h2>
 			<h3>{{ project.name }}</h3>
-			<header>
+			<div class="project__header">
 				<div v-if="project.image_url || project.video_url" class="project__thumbnail" :class="'project__thumbnail--' + (project.video_url != null ? 'video' : 'image')">
 					<iframe v-if="project.video_url" :src="project.video_url" width="560" height="315" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 					<img v-else :src="project.image_url" :alt="project.image_alt">
 				</div>
-				<div class="project__header">
+				<div class="header__content">
 					<p><strong>{{ project.summary.captured_count }}</strong> pessoas já ajudaram</p>
 					<p><strong>{{ getRemainingDays(project.end_ts) }}</strong> dias restantes</p>
 
@@ -33,7 +33,7 @@
 						QUERO AJUDAR
 					</a>
 				</div>
-			</header>
+			</div>
 			<div class="project__content" v-html="project.description"></div>
 			<footer v-if="project.legal_name || project.legal_document" class="project__footer">
 				<dl class="legal-info">
@@ -43,7 +43,7 @@
 					<dd v-if="project.legal_document">{{ project.legal_document | formatCNPJ }}</dd>
 				</dl>
 			</footer>
-		</section>
+		</article>
 	</main>
 </template>
 
